57672ca4053323d5701f17fae8717c77b3c9a4fe
psychic94 <psychic94@gmail.com>
2012-12-31 23:32:48 -0500

    Update and new method
    Renamed to fit new language name
    Added better commenting
    Added render method

---
+++ java/sigmal/lexer/SiGMALObject.java
@@ -0,0 +1,29 @@
+package sigmal.lexer;
+
+import java.util.ArrayList;
+
+//Used as a template for objects
+public class SiGMALObject{
+    //The name of the object type
+    private final String type;
+    //This instance's identifier
+    private final String name;
+    //The parameters used in creating this object
+    private final String params;
+    //The code block from the original program file
+    private final ArrayList<String> definition;
+    //The list of sub parts this instance has
+    private ArrayList<SiGMALObject> members;
+    
+    public SiGMALObject(String type, String name, ArrayList definition){
+        this.type = type;
+        this.name = name;
+        this.params = (String)definition.remove(0);
+        this.definition = definition;
+    }
+    
+    //Notify the environment to draw this instance
+    public void render(){
+        GraphicsHandler.renderObject(this);
+    }
+}
