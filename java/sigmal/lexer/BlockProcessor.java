208ba2b66c72ec143fdd05e8966cfe4db52735ee
psychic94 <psychic94@gmail.com>
2012-12-31 23:25:59 -0500

    Adding parts
    Added "when" as new block starter
    Added code to process final block
    Moved code for processing blocks into new method

--- java/sigmal/lexer/BlockProcessor.java
+++ java/sigmal/lexer/BlockProcessor.java
@@ -23,7 +23,7 @@ public class BlockProcessor{
         // Make a list of the lines that start each block
         for(int i=0; i<lines.size(); i++){
             line = lines.get(i).trim();
-            if(line.startsWith("object") || line.startsWith("envir") || line.startsWith("action"))
+            if(line.startsWith("object") || line.startsWith("envir") || line.startsWith("action") || line.startsWith("when"))
                 blocks.add(i);
         }
         lines.trimToSize();
@@ -37,11 +37,25 @@ public class BlockProcessor{
                     block.add(lines.get(j));
             }
             block.trimToSize();
-            if(block.get(0).startsWith("object")){
-                String type = block.get(0).substring(0, block.get(0).indexOf("[")).split(" ")[1];
-                block.set(0, block.get(0).substring(block.get(0).indexOf("[")+1, block.get(0).indexOf("]")));
-                SADLObjectFactory.types.put(type, block);
-            }
+            processBlock(block);
+        }
+        block = new ArrayList();
+        for(int j=blocks.get(blocks.size()-1); j<lines.size(); j++){
+                if(!lines.get(j).trim().equals(""))
+                    block.add(lines.get(j));
+        }
+        block.trimToSize();
+        processBlock(block);
+    }
+        
+    private void processBlock(ArrayList<String> block){
+        if(block.get(0).startsWith("object")){
+            //The type name is the second word of whats before the brackets
+            String type = block.get(0).substring(0, block.get(0).indexOf("[")).split(" ")[1];
+            //Set the first line to whats in the brackets
+            block.set(0, block.get(0).substring(block.get(0).indexOf("[")+1, block.get(0).indexOf("]")));
+            //Register the object type
+            SiGMALObjectFactory.types.put(type, block);
         }
     }
 }
