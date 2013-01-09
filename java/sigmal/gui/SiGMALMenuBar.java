babe1914c62de01e860d2d1d2fafeb57396e14fb
psychic94 <psychic94@gmail.com>
2013-01-08 21:16:25 -0500

    Added an export section

--- java/sigmal/gui/SiGMALMenuBar.java
+++ java/sigmal/gui/SiGMALMenuBar.java
@@ -10,6 +10,7 @@ public class SiGMALMenuBar extends JMenuBar{
     JMenu file;
     JMenu edit;
     JMenu tools;
+    JMenu export;
     public SiGMALMenuBar(){
         super();
         file = new JMenu("File");
@@ -18,11 +19,35 @@ public class SiGMALMenuBar extends JMenuBar{
         file.add(new JMenuItem("New", KeyEvent.VK_N));
         file.add(new JMenuItem("Open", KeyEvent.VK_O));
         file.add(new JMenuItem("Save", KeyEvent.VK_S));
+        export = createExportTree();
+        file.add(export);
         tools.add(new JMenuItem("Find", KeyEvent.VK_F));
         tools.add(new JMenuItem("Preferences"));
         add(file);
         add(edit);
         add(tools);
         setOpaque(true);
+    }
+    
+    private JMenu createExportTree(){
+        JMenu expimage = new JMenu("Image");
+        expimage.add(new JMenuItem("JPEG"));
+        expimage.add(new JMenuItem("GIF"));
+        
+        JMenu expvideo = new JMenu("Video");
+        expvideo.add(new JMenuItem("AVI"));
+        expvideo.add(new JMenuItem("FLV"));
+        expvideo.add(new JMenuItem("MNG"));
+        expvideo.add(new JMenuItem("MOV"));
+        expvideo.add(new JMenuItem("WMV"));
+        
+        JMenu expmodel = new JMenu("Model");
+        
+        JMenu menu = new JMenu("Export");
+        menu.add(expimage);
+        menu.add(expvideo);
+        //menu.add(expmodel);
+        
+        return menu;
     }
 }
