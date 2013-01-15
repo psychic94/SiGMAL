a642b972ab87fde5d6747269f3944aa419d06beb
psychic94 <psychic94@gmail.com>
2013-01-15 11:24:56 -0800

    Added SiGMALCamera

---
+++ java/sigmal/lexer/SiGMALCamera.java
@@ -0,0 +1,25 @@
+package sigmal.lexer;
+
+public class SiGMALCamera{
+    private Point pos;
+    private double theta;
+    private double phi;
+
+    public SiGMALCamera(){
+        pos = new Point(0, 0, 0);
+        theta = 0;
+        phi = 0;
+    }
+
+    public Point getPos(){
+        return pos;
+    }
+
+    public double getTheta(){
+        return theta;
+    }
+
+    public double getPhi(){
+        return phi;
+    }
+}
