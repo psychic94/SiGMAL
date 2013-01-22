93ac91d7f767cf5bb8429cb0eb88235973edc979
psychic94 <psychic94@gmail.com>
2013-01-22 14:26:18 -0500

    Internalized spherical coordinates

--- java/sigmal/utils/Point.java
+++ java/sigmal/utils/Point.java
@@ -1,14 +1,22 @@
 package sigmal.utils;
 
 public class Point{
+    //Cartesian coordinates
     private final double x, y, z, w;
+    //Spherical coordinates
+    private final double theta, phi, omicron, r;
     //2D 3D or 4D
     private final double dims;
     public Point(double x, double y){
         this(x, y, 0, 0, 2);
+        this.r = Math.sqrt(x*x + y*y);
+        this.theta = Math.atan2(y, x);
     }
     public Point(double x, double y, double z){
         this(x, y, z, 0, 3);
+        this.r = Math.sqrt(x*x + y*y + z*z);
+        this.phi = Math.atan(y/x);
+        this.theta = Math.acos(z/r);
     }
     public Point(double x, double y, double z, double w){
         this(x, y, z, w, 4);
@@ -30,7 +38,26 @@ public class Point{
     public double getZ(){
         return z;
     }
+    public double getW(){
+        return w;
+    }
+    public double getTheta(){
+        return theta;
+    }
+    public double getPhi(){
+        return phi;
+    }
+    public double getOmicron(){
+        return omicron;
+    }
+    public double getR(){
+        return r;
+    }
     public double getDims(){
         return dims;
+    }
+    
+    public Point relativeTo(Point pt){
+        return new Point(x-pt.getX(), y-pt.getY(), z-pt.getZ(), w-pt.getW());
     }
 }
