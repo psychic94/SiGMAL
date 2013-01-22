2d6ec5ee26069e95f0f1ebff2c72f8aef008d152
psychic94 <psychic94@gmail.com>
2013-01-22 14:39:49 -0500

    Utilized Point's relativeTo method

--- java/sigmal/lexer/GraphicsHandler.java
+++ java/sigmal/lexer/GraphicsHandler.java
@@ -8,31 +8,7 @@ public class GraphicsHandler{
     }
     
     public static Point projectTo2D(Point pt, SiGMALCamera cam){
-        pt = toSpherical(pt, cam.getPos());
-        //The line above stored theta as x, phi as y, and r as z
-        double theta = pt.getX() - cam.getTheta();
-        double phi = pt.getY() - cam.getPhi();
-        double r = pt.getZ();
-        if(r==0) return new Point(0, 0);
-        pt = toCartesian(theta, phi, r);
+        pt = pt.relativeTo(cam.getPos(), cam.getDir());
         return new Point(pt.getX() / r, pt.getY() / r);
-    }
-    
-    public static Point toSpherical(Point pt, Point center){
-        return toSpherical(pt.getX()-center.getX(), pt.getY()-center.getY(), pt.getZ()-center.getZ());
-    }
-    
-    public static Point toSpherical(double x, double y, double z){
-        double r = Math.sqrt(x*x + y*y + z*z);
-        double phi = Math.atan(y/x);
-        double theta = Math.acos(z/r);
-        return new Point(theta, phi, r);
-    }
-    
-    public static Point toCartesian(double theta, double phi, double r){
-        double x = r * Math.sin(theta) * Math.cos(phi);
-        double y = r * Math.sin(theta) * Math.sin(phi);
-        double z = r * Math.cos(theta);
-        return new Point(x ,y, z);
     }
 }
