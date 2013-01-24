c6b8b533c5642042a1fd2ee0c468e1a9d4cbe4ab
psychic94 <psychic94@gmail.com>
2013-01-24 14:27:02 -0500

    Renamed variables and accessors

--- java/sigmal/lexer/SiGMALCamera.java
+++ java/sigmal/lexer/SiGMALCamera.java
@@ -4,24 +4,24 @@ import sigmal.utils.Point;
 
 public class SiGMALCamera{
     private Point pos;
-    private double theta;
-    private double phi;
+    private double yaw;
+    private double pitch;
 
     public SiGMALCamera(){
         pos = new Point(0, 0, 0);
-        theta = 0;
-        phi = 0;
+        yaw = 0;
+        pitch = 0;
     }
 
     public Point getPos(){
         return pos;
     }
 
-    public double getTheta(){
-        return theta;
+    public double getYaw(){
+        return yaw;
     }
 
-    public double getPhi(){
-        return phi;
+    public double getPitch(){
+        return pitch;
     }
 }
