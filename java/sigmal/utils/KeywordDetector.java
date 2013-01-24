e23d5785c354ec2f795bd8a6aab6f67d3e0b7789
psychic94 <psychic94@gmail.com>
2013-01-24 13:42:24 -0500

    Added more tests, a new logic keyword, and a type

--- java/sigmal/utils/KeywordDetector.java
+++ java/sigmal/utils/KeywordDetector.java
@@ -17,7 +17,24 @@ public class KeywordDetector{
         else return (word.equalsIgnoreCase("if") || word.equalsIgnoreCase("else") || 
                      word.equalsIgnoreCase("regardless") || word.equalsIgnoreCase("rgdls") || 
                      word.equalsIgnoreCase("repeat") || word.equalsIgnoreCase("while") || 
-                     word.equalsIgnoreCase("until"));
+                     word.equalsIgnoreCase("until") || word.equalsIgnoreCase("unless"));
+    }
+    
+    public static boolean isValue(String word){
+        word.trim();
+        if(word.split(" ").length > 1) return false;
+        else if(word.equalsIgnoreCase("true") || word.equalsIgnoreCase("false") ||
+                word.equalsIgnoreCase("nothing") || word.equalsIgnoreCase("NA")) return true;
+        else{
+            //If it can be parsed as a number it's a value, otherwise it's not
+            //There is no need to store the Integer because the test is pass/fail
+            try{
+                new Integer(word);
+            }catch(NumberFormatException e){
+                return false;
+            }
+            return true;
+        }
     }
     
     public static boolean isKnownModifier(String word){
@@ -33,7 +50,11 @@ public class KeywordDetector{
     }
     
     public static Type getKeywordType(String word){
-        return Type.blockStart;
+        if(isBlockStartKeyword(word)) return Type.blockStart;
+        else if(isLogicKeyword(word)) return Type.logic;
+        else if(isValue(word)) return Type.value;
+        else if(isKnownModifier(word)) return Type.modifier;
+        else return Type.none;
     }
     
     public static enum Type{
@@ -44,5 +65,6 @@ public class KeywordDetector{
         object,
         envir,
         action,
+        none
     }
 }
