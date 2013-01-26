a72f843507f385e065093e01754fdc0246d509d9
psychic94 <psychic94@gmail.com>
2013-01-26 14:53:37 -0500

    Swapped theta and phi; overloaded relativeTo

--- java/sigmal/utils/Point.java
+++ java/sigmal/utils/Point.java
@@ -3,10 +3,10 @@ package sigmal.utils;
 public class Point{
     //Cartesian coordinates
     private final double x, y, z, w;
-    //Spherical coordinates
+    //Spherical coordinates; theta is yaw, phi is pitch, omicron is roll(4D)
     private final double theta, phi, omicron, r;
     //2D 3D or 4D
-    private final double dims;
+    private final int dims;
     public Point(double x, double y){
         this.x = x;
         this.y = y;
@@ -24,8 +24,8 @@ public class Point{
         this.z = z;
         this.w = 0;
         this.r = Math.sqrt(x*x + y*y + z*z);
-        this.phi = Math.atan(y/x);
-        this.theta = Math.acos(z/r);
+        this.theta = Math.atan(y/x);
+        this.phi = Math.acos(z/r);
         this.omicron = 0;
         this.dims = 3;
     }
@@ -65,11 +65,31 @@ public class Point{
     public double getR(){
         return r;
     }
-    public double getDims(){
+    public int getDims(){
         return dims;
     }
     
     public Point relativeTo(Point pt){
         return new Point(x-pt.getX(), y-pt.getY(), z-pt.getZ(), w-pt.getW());
+    }
+    
+    //This is for 2D use only
+    public Point relativeTo(Point pt, double yaw){
+        pt = this.relativeTo(pt);
+        yaw = pt.getTheta() - yaw;
+        double x2 = pt.getR() * Math.cos(yaw);
+        double y2 = pt.getR() * Math.sin(yaw); 
+        return new Point(x2, y2);
+    }
+    
+    //This is for 3D use only
+    public Point relativeTo(Point pt, double yaw, double pitch){
+        pt = this.relativeTo(pt);
+        yaw = pt.getTheta() - yaw;
+        pitch = pt.getPhi() - pitch;
+        double x2 = pt.getR() * Math.sin(pitch) * Math.cos(yaw);
+        double y2 = pt.getR() * Math.sin(pitch) * Math.sin(yaw);
+        double z2 = pt.getR() * Math.cos(pitch);        
+        return new Point(x2, y2, z2);
     }
 }
