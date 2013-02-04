cc52aa544ee78a11abdb376d5cc630f3b9454309
psychic94 <psychic94@gmail.com>
2013-02-04 14:01:36 -0500

    Added roll

--- java/sigmal/lexer/SiGMALCamera.java
+++ java/sigmal/lexer/SiGMALCamera.java
@@ -6,22 +6,25 @@ public class SiGMALCamera{
     private Point pos;
     private double yaw;
     private double pitch;
+    private double roll;
 
     public SiGMALCamera(){
-        pos = new Point(0, 0, 0);
+        pos = new Point(0, 0, 0, 0);
         yaw = 0;
         pitch = 0;
+        roll = 0;
     }
 
     public Point getPos(){
         return pos;
     }
-
     public double getYaw(){
         return yaw;
     }
-
     public double getPitch(){
         return pitch;
+    }
+    public double getRoll(){
+        return roll;
     }
 }
