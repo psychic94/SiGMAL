0f861f4f7a5abd5d7f326695d19c9d295b5f0fba
psychic94 <psychic94@gmail.com>
2013-02-04 13:32:16 -0500

    Implemented angles in 4D
    getR now calculates radius at runtime using an int representing the number of dimensions to factor in

--- java/sigmal/utils/Point.java
+++ java/sigmal/utils/Point.java
@@ -4,7 +4,7 @@ public class Point{
     //Cartesian coordinates
     private final double x, y, z, w;
     //Spherical coordinates; theta is yaw, phi is pitch, omicron is roll(4D)
-    private final double theta, phi, omicron, r;
+    private final double theta, phi, omicron;
     //2D 3D or 4D
     private final int dims;
     public Point(double x, double y){
@@ -12,7 +12,6 @@ public class Point{
         this.y = y;
         this.z = 0;
         this.w = 0;
-        this.r = Math.sqrt(x*x + y*y);
         this.theta = Math.atan2(y, x);
         this.phi = 0;
         this.omicron = 0;
@@ -23,9 +22,8 @@ public class Point{
         this.y = y;
         this.z = z;
         this.w = 0;
-        this.r = Math.sqrt(x*x + y*y + z*z);
-        this.theta = Math.atan(y/x);
-        this.phi = Math.acos(z/r);
+        this.theta = Math.atan2(y, x);
+        this.phi = Math.atan2(z, getR(2));
         this.omicron = 0;
         this.dims = 3;
     }
@@ -34,10 +32,9 @@ public class Point{
         this.y = y;
         this.z = z;
         this.w = w;
-        this.r = Math.sqrt(x*x + y*y + z*z + w*w);
-        this.theta = 0;
-        this.phi = 0;
-        this.omicron = 0;
+        this.theta = Math.atan2(y, x);
+        this.phi = Math.atan2(z, getR(2));
+        this.omicron = Math.atan2(w, getR(3));
         this.dims = 4;
     }
     
@@ -62,8 +59,12 @@ public class Point{
     public double getOmicron(){
         return omicron;
     }
-    public double getR(){
-        return r;
+    public double getR(int dims){
+        if(dims<=0) return 0;
+        else if(dims==1) return x;
+        else if(dims==2) return Math.sqrt(x*x+y*y);
+        else if(dims==3) return Math.sqrt(x*x+y*y+z*z);
+        else return Math.sqrt(x*x+y*y+z*z+w*w);
     }
     public int getDims(){
         return dims;
@@ -77,8 +78,8 @@ public class Point{
     public Point relativeTo(Point pt, double yaw){
         pt = this.relativeTo(pt);
         yaw = pt.getTheta() - yaw;
-        double x2 = pt.getR() * Math.cos(yaw);
-        double y2 = pt.getR() * Math.sin(yaw); 
+        double x2 = pt.getR(2) * Math.cos(yaw);
+        double y2 = pt.getR(2) * Math.sin(yaw); 
         return new Point(x2, y2);
     }
     
@@ -87,9 +88,9 @@ public class Point{
         pt = this.relativeTo(pt);
         yaw = pt.getTheta() - yaw;
         pitch = pt.getPhi() - pitch;
-        double x2 = pt.getR() * Math.sin(pitch) * Math.cos(yaw);
-        double y2 = pt.getR() * Math.sin(pitch) * Math.sin(yaw);
-        double z2 = pt.getR() * Math.cos(pitch);        
+        double x2 = pt.getR(3) * Math.sin(pitch) * Math.cos(yaw);
+        double y2 = pt.getR(3) * Math.sin(pitch) * Math.sin(yaw);
+        double z2 = pt.getR(3) * Math.cos(pitch);        
         return new Point(x2, y2, z2);
     }
 }
