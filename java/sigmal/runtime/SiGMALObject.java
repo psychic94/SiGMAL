2ea3bb3553381490316518576dc5b9fae033daa9
psychic94 <psychic94@gmail.com>
2013-02-13 15:30:12 -0500

    Added more methods

--- java/sigmal/runtime/SiGMALObject.java
+++ java/sigmal/runtime/SiGMALObject.java
@@ -4,15 +4,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
+import sigmal.utils.*;
+
 //Used as a template for objects
 public abstract class SiGMALObject{
     //The list of sub parts this instance has
-    private ArrayList<SiGMALObject> members;
+    protected HashMap<SiGMALObject> members;
+    protected ArrayList<Point> points;
     
     //Notify the environment to draw this instance
     public abstract void render();
     
-    public void addMember(SiGMALObject member){
-        members.add(member);
+    //Associates an object to this objce
+    public void addMember(String name, SiGMALObject member){
+        members.put(name, member);
     }
+    
+    //Removes association to the member object and associates the ex-member with the environment
+    public abstract void removeMember(String name);
+    
+    //Deletes the member object and its members
+    public abstract void deleteMember(String name);
+    
+    //Rotates the object about a line
+    //Axis>0:clockwise,axis<0:counterclockwise
+    //Clockwise is defined by the perspective from the point specified in the ray looking in the diraction of the ray
+    public abstract rotate(Ray axis, double angle);
 }
